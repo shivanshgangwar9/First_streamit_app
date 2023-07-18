351142b91c8263b1c8a9eeaa77de964f520877b3
shivanshgangwar9 <85667052+shivanshgangwar9@users.noreply.github.com>
2023-07-18 12:14:27 +0530

    Update streamlit_app.py
    adding emoj

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('my parent new healthy dine')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(' 🐔Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🍞 Hard-Boiled Free-Range Egg')
+
+
